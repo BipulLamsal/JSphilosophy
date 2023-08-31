29387b9b68b299215a85d18822418537ca293438
BipulLamsal <bipullamsal@gmail.com>
2023-08-31 17:21:46 +0545

    git morgan middleware

--- PhoneBook/app.js
+++ PhoneBook/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const external = require("./randomize");
-
+//moragn - http request logger middleware for nodejs
+const moragn = require("morgan");
+const morgan = require("morgan");
 
 let record = [
   {
@@ -27,11 +29,23 @@ let record = [
   },
 ];
 
-
 const findNumber = () => {
   return record.length;
 };
 app.use(express.json());
+
+moragn.token("person_name", function (req, res) {
+  return req.body.name;
+});
+moragn.token("person_number", function (req, res) {
+  return req.body.number;
+});
+morgan.token("person_info", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :response-time :person_info :person_name :person_number")
+);
 app.get("/api/persons", (req, res) => {
   res.json(record);
 });
@@ -68,14 +82,11 @@ app.post("/api/persons/", (req, res) => {
     return res.status(400).json({
       error: "both name and contact missing.",
     });
-  } 
-  else if (record.find((item)=>item.name == body.name))
-  {
+  } else if (record.find((item) => item.name == body.name)) {
     return res.status(400).json({
-      error : "name already exists"
+      error: "name already exists",
     });
-  }
-  else {
+  } else {
     const detail = {
       id: external.randomizer(),
       name: body.name,
@@ -85,7 +96,6 @@ app.post("/api/persons/", (req, res) => {
     res.json(detail);
   }
 });
-
 app.delete("/api/persons/:id", (req, res) => {
   const personId = Number(req.params.id);
   record = record.filter((item) => {
