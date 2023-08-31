95208dac839a360089e1fc7728c180db0614e61e
BipulLamsal <bipullamsal@gmail.com>
2023-08-31 16:29:59 +0545

    crud using rest api

--- PhoneBook/app.js
+++ PhoneBook/app.js
@@ -1,77 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const external = require('./randomize')
-
-const findNumber = () =>{
-    return record.length;
-} 
-app.use(express.json());
-app.get('/api/persons',(req,res)=>{
-res.json(record);
-})
-
-app.get('/api/info',(req,res)=>{
-    const numberOfRecords = findNumber();
-    const currentDate =  new Date();
-    res.send(`Number of records: ${numberOfRecords}</br>Current Data:${currentDate} `)
-})
-
-app.get('/api/persons/:id',(req,res)=>{
-    const personId = req.params.id;
-    const person = record.filter((item)=> personId ==item.id);
-    if (person.length == 0)
-    {
-        console.log("hi")
-        return res.status(404).send();
-    }
-    res.json(person);
-})
+const external = require("./randomize");
 
 
-app.post('/api/persons/',(req,res)=>{
-const body = req.body;
-if(!body.name)
-{
-  return res.status(400).json({
-    error: "name missing"
-  })
-}
-else if (!body.number)
-{
-  return res.status(400).json({
-    error: "contact missing"
-  })
-}
-else if (!body.number && !body.name)
-{
-  return res.status(400).json({
-    error: "both name and contact missing."
-  })
-}
-else
-{
-  const detail  = {
-    id: external.randomizer(),
-    name: body.name,
-    number: body.number
-  }
-  record.push(detail);
-res.json(detail)
-}
-})
-
-app.delete('/api/persons/:id',(req,res)=>{
-    const personId = Number(req.params.id);
-    record = record.filter((item)=>{
-        return item.id !== personId
-    })
-    res.status(204).send();
-})
-//starting the server
-app.listen(port,(req,res)=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
 let record = [
   {
     id: 1,
@@ -94,3 +26,74 @@ let record = [
     number: "39-23-6423122",
   },
 ];
+
+
+const findNumber = () => {
+  return record.length;
+};
+app.use(express.json());
+app.get("/api/persons", (req, res) => {
+  res.json(record);
+});
+
+app.get("/api/info", (req, res) => {
+  const numberOfRecords = findNumber();
+  const currentDate = new Date();
+  res.send(
+    `Number of records: ${numberOfRecords}</br>Current Data:${currentDate} `
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const personId = req.params.id;
+  const person = record.filter((item) => personId == item.id);
+  if (person.length == 0) {
+    console.log("hi");
+    return res.status(404).send();
+  }
+  res.json(person);
+});
+
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: "contact missing",
+    });
+  } else if (!body.number && !body.name) {
+    return res.status(400).json({
+      error: "both name and contact missing.",
+    });
+  } 
+  else if (record.find((item)=>item.name == body.name))
+  {
+    return res.status(400).json({
+      error : "name already exists"
+    });
+  }
+  else {
+    const detail = {
+      id: external.randomizer(),
+      name: body.name,
+      number: body.number,
+    };
+    record.push(detail);
+    res.json(detail);
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const personId = Number(req.params.id);
+  record = record.filter((item) => {
+    return item.id !== personId;
+  });
+  res.status(204).send();
+});
+//starting the server
+app.listen(port, (req, res) => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
